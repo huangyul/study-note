1d09d469d45bf6516719a2fde7aae934f841abc5
huangyul <1103221645@qq.com>
2023-01-06 15:01:03 +0800

    有效括号

--- docs/algorithm/test.js
+++ docs/algorithm/test.js
@@ -1,45 +1,27 @@
-function ListNode(val, next) {
-  this.val = val === undefined ? 0 : val
-  this.next = next === undefined ? null : next
+function func(s) {
+  if (s.length === 0) {
+    return false
+  }
+  if (s.length % 2 === 1) {
+    return false
+  }
+
+  let map = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
+  }
+  const res = []
+  for (let i = 0; i < s.length; i++) {
+    if (res.length === 0) {
+      res.push(s[i])
+    } else if (res[res.length - 1] === map[s[i]]) {
+      res.pop()
+    } else {
+      res.push(s[i])
+    }
+  }
+  return res.length === 0
 }
 
-// function func() {
-//   let list = new ListNode()
-//   list.val = 1
-//   let head = list
-//   list.next = new ListNode(2)
-//   list = list.next
-//   console.log(list)
-//   console.log(head)
-// }
-
-function generateList(arr) {
-  let list = new ListNode(0)
-  let head = list
-  for (let i = 0; i < arr.length; i++) {
-    list.next = new ListNode(arr[i])
-    list = list.next
-  }
-  return head.next
-}
-
-function func(head, n) {
-  head = generateList(head)
-  let ret = new ListNode(0, head)
-  let slow = ret
-  let fast = ret
-
-  while (n--) {
-    fast = fast.next
-  }
-  if (!fast) return ret.next
-
-  while (fast.next) {
-    fast = fast.next
-    slow = slow.next
-  }
-  slow.next = slow.next.next
-  return ret.next
-}
-
-console.log(func([1, 2, 3, 4, 5], 4))
+console.log(func('()()'))
