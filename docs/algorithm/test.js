0307d5b76756dd7e24c09c076fd59bb5d7de237d
huangyul <1103221645@qq.com>
2023-01-16 17:18:02 +0800

    continue

--- docs/algorithm/test.js
+++ docs/algorithm/test.js
@@ -25,27 +25,23 @@ function generateListNode(arr) {
  */
 //  输入：nums = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat']
 //  输出：[["bat"],["nat","tan"],["ate","eat","tea"]]
-// function func(strs) {
-//   var h = new Map(),
-//     k
-//   for (var i = 0; i < strs.length; i++) {
-//     k = strs[i].split('').sort().join('')
-//     h.has(k) ? h.get(k).push(strs[i]) : h.set(k, [strs[i]])
-//   }
-
-//   return Array.from(h.values())
-// }
-
-function func(strs) {
-  let k,
-    h = new Map()
-  for (let i = 0; i < strs.length; i++) {
-    k = strs[i].split('').sort().join('')
-    h.has(k) ? h.get(k).push(strs[i]) : h.set(k, [strs[i]])
+function func(nums) {
+  if (!nums.length) {
+    return
   }
-  console.log(h.values())
-  return Array.from(h.values())
+  // 在每一个扫描点计算以该点数值为结束点的子数列的最大和（正数和）。
+  let max_ending_here = nums[0]
+  let max_so_far = nums[0]
+
+  for (let i = 1; i < nums.length; i++) {
+    // 以每个位置为终点的最大子数列 都是基于其前一位置的最大子数列计算得出,
+
+    max_ending_here = Math.max(nums[i], max_ending_here + nums[i])
+    max_so_far = Math.max(max_so_far, max_ending_here)
+  }
+
+  return max_so_far
 }
 
-const res = func(['eat', 'tea', 'tan', 'ate', 'nat', 'bat'])
+const res = func([-2, 1, -3, 4, -1, 2, 1, -5, 4])
 console.log(res)
