754596351c857c6041abab2098df0f11bc9371b6
yul <1103221645@qq.com>
2023-02-03 20:11:18 +0800

    update

--- docs/algorithm/test.js
+++ docs/algorithm/test.js
@@ -23,32 +23,15 @@ function generateListNode(arr) {
  * 上面是ListNode链表类 generateListNode(arr)
  * 下面才是正式代码编写的开始
  */
-//  输入：nums = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat']
-//  输出：[["bat"],["nat","tan"],["ate","eat","tea"]]
-function func(n) {
-  let start = Date.now()
-  // if (n <= 2) {
-  //   return n
-  // }
-  // const pd = []
-  // pd[0] = 1
-  // pd[1] = 2
-  // for (let i = 2; i < n; i++) {
-  //   pd[i] = pd[i - 1] + pd[i - 2]
-  // }
-  // console.log(Date.now() - start)
-  // return pd[n - 1]
-
-
-  function func1(a, b, n) {
-    if (n === 0) return b
-    return func1(b, a + b, n - 1)
+function func(nums) {
+  let n = nums.length - 1
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] >= n - 1) {
+      n = i
+    }
   }
-  const res = func1(0, 1, n)
-  console.log(Date.now() - start)
-  return res
-
+  return n === 0
 }
 
-const res = func(45)
+const res = func([2, 0, 0, 1, 4])
 console.log(res)
