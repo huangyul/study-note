a2d226380b97a128f9c28b430e3c4547e313949f
yul <1103221645@qq.com>
2023-02-05 13:55:32 +0800

    update

--- docs/algorithm/test.js
+++ docs/algorithm/test.js
@@ -26,16 +26,18 @@ function generateListNode(arr) {
 function func(nums) {
   const res = []
 
-  const dfs = (index, list) => {
-    res.push(list.slice()) // 调用子递归前，加入解集
+  function dfs(index, list) {
+    res.push([...list])
+
     for (let i = index; i < nums.length; i++) {
-      // 枚举出所有可选的数
-      list.push(nums[i]) // 选这个数
-      dfs(i + 1, list) // 基于选这个数，继续递归，传入的是i+1，不是index+1
-      list.pop() // 撤销选这个数
+      list.push(nums[i])
+      dfs(i + 1, list)
+      list.pop() // 回溯
     }
   }
+
   dfs(0, [])
+
   return res
 }
 
