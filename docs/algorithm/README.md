7ade2f60e68d2cf63723b3f0ce65526634b823e3
huangyul <1103221645@qq.com>
2023-02-13 16:32:22 +0800

    二叉树的层序遍历

--- docs/algorithm/README.md
+++ docs/algorithm/README.md
@@ -1789,3 +1789,48 @@ function func(root) {
   return compareNode(root.left, root.right)
 }
 ```
+
+## 二叉树的层序遍历
+
+> 中等
+
+### 题目描述
+
+给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+
+#### 输出示例
+
+输入：root = [3,9,20,null,null,15,7]
+输出：[[3],[9,20],[15,7]]
+
+输入：root = [1]
+输出：[[1]]
+
+### 解法-层序遍历
+
+主要的思路是使用广度优先搜索，并且使用队列的性质，先进先出，将同一层的数据放到同一组中
+
+```js
+function func(root) {
+  let res = []
+  let queue = [] // 队列
+  if (root === null) {
+    return res
+  }
+  // 将整个二叉树推进队列，以后每次遍历队列的长度，就只会遍历同一层的值
+  queue.push(root)
+  while (queue.length !== 0) {
+    let curLevel = [] // 用来存储同一层的值
+    let len = queue.length // 因为后面会新推入值，所以要先将长度（同一层的长度）存起来
+    for (let i = 0; i < len; i++) {
+      let node = queue.shift() // 从头取出第一个
+      curLevel.push(node.val)
+      // 如果取出的值有左子树或右子树，推入队列中，用于下一轮遍历，注意先进先出
+      node.left && queue.push(node.left)
+      node.right && queue.push(node.right)
+    }
+    res.push(curLevel)
+  }
+  return res
+}
+```
--- docs/algorithm/test.js
+++ docs/algorithm/test.js
@@ -25,41 +25,33 @@ function generateListNode(arr) {
 /**
  * 二叉树
  */
-class node {
-  constructor(val) {
-    this.key = val
+class Node {
+  // 定义节点
+  constructor(data) {
+    this.val = data
     this.left = null
     this.right = null
   }
 }
-class binaryTree {
-  constructor() {
-    this.root = null
-  }
 
-  insert(val) {
-    let newNode = new node(val)
-    if (this.root === null) {
-      this.root = newNode
-    } else {
-      this.insertNode(this.root, newNode)
-    }
+const createTree = (arr) => {
+  if (arr.length === 0) {
+    return null
   }
-
-  insertNode(root, curNode) {
-    if (root.key > curNode.key) {
-      if (root.left === null) {
-        root.left = curNode
-      } else {
-        this.insertNode(root.left, curNode)
-      }
-    } else {
-      if (root.right === null) {
-        root.right = curNode
-      } else {
-        this.insertNode(root.right, curNode)
-      }
-    }
+  // 创建二叉树
+  let tree = new Node(arr[0])
+  let Nodes = [tree]
+  let i = 1
+  if (i == arr.length) {
+    return tree
+  }
+  for (let node of Nodes) {
+    Nodes.push((node.left = new Node(arr[i])))
+    i += 1
+    if (i == arr.length) return tree
+    Nodes.push((node.right = new Node(arr[i])))
+    i += 1
+    if (i == arr.length) return tree
   }
 }
 
@@ -68,17 +60,28 @@ class binaryTree {
  * 下面才是正式代码编写的开始
  */
 
-function func(n) {
-  let dp = new Array(n + 1).fill(0)
-  dp[0] = 1
-  dp[1] = 1
-  for (let i = 2; i <= n; i++) {
-    for (let j = 1; j <= i; j++) {
-      dp[i] += dp[j - 1] * dp[i - j]
-    }
+function func(root) {
+  let res = []
+  let queue = [] // 队列
+  if (root === null) {
+    return res
   }
-  return dp[n]
+  // 将整个二叉树推进队列，以后每次遍历队列的长度，就只会遍历同一层的值
+  queue.push(root)
+  while (queue.length !== 0) {
+    let curLevel = [] // 用来存储同一层的值
+    let len = queue.length // 因为后面会新推入值，所以要先将长度（同一层的长度）存起来
+    for (let i = 0; i < len; i++) {
+      let node = queue.shift() // 从头取出第一个
+      curLevel.push(node.val)
+      // 如果取出的值有左子树或右子树，推入队列中，用于下一轮遍历，注意先进先出
+      node.left && queue.push(node.left)
+      node.right && queue.push(node.right)
+    }
+    res.push(curLevel)
+  }
+  return res
 }
 
-const res = func(3)
+const res = func(createTree([3, 9, 20, null, null, 15, 7]))
 console.log(res)
