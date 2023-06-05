73ee0614f9f51fa9a6c49d2304117ff9ea60a72d
huangyul <1103221645@qq.com>
2023-06-05 16:13:30 +0800

    环形链表2

--- docs/go/al_test.go
+++ docs/go/al_test.go
@@ -171,3 +171,33 @@ func TestGetIntersectionNode(t *testing.T) {
 	}
 	return pA
 }
+
+/* 6.5 */
+// No.142
+func TestDetectCycle(t *testing.T) {
+	head := &ListNode{Val: 1}
+	node2 := &ListNode{Val: 2}
+	node3 := &ListNode{Val: 3}
+	node4 := &ListNode{Val: 4}
+	node5 := &ListNode{Val: 5}
+
+	head.Next = node2
+	node2.Next = node3
+	node3.Next = node4
+	node4.Next = node5
+
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			slow = head
+			for slow != fast {
+				slow = slow.Next
+				fast = fast.Next
+			}
+			return slow
+		}
+	}
+	return nil
+}
