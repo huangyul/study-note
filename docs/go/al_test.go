d7e2e37a36d5a9530716246eae14d4f20b572969
huangyul <1103221645@qq.com>
2023-06-07 16:45:05 +0800

    四树之和

--- docs/go/al_test.go
+++ docs/go/al_test.go
@@ -236,3 +236,45 @@ func TestThreeSum(t *testing.T) {
 	}
 	fmt.Println(res)
 }
+
+/* 6.7 */
+// No.18
+func TestFourSum(t *testing.T) {
+	nums := []int{1, 0, -1, 0, -2, 2}
+	target := 0
+
+	sort.Ints(nums)
+	res := [][]int{}
+	n := len(nums)
+
+	for i := 0; i < n-3; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		for j := i + 1; j < n-2; j++ {
+			if j > 0 && nums[j] == nums[j-1] {
+				continue
+			}
+			left, right := j+1, n-1
+			for left < right {
+				sum := nums[i] + nums[j] + nums[left] + nums[right]
+				if sum == target {
+					res = append(res, []int{nums[i], nums[j], nums[left], nums[right]})
+					for left < right && nums[left] == nums[left+1] {
+						left++
+					}
+					for left < right && nums[right] == nums[right-1] {
+						right--
+					}
+					left++
+					right--
+				} else if sum < target {
+					left++
+				} else {
+					right--
+				}
+			}
+		}
+	}
+	fmt.Println(res)
+}
