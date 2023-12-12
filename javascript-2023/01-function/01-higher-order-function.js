6e72833d73d8db4f64355ea490b2153cfde4dac5
huanghyul <1103221645@qq.com>
2023-12-12 20:53:02 +0800

    docs: update

--- javascript-2023/01-function/01-higher-order-function.js
+++ javascript-2023/01-function/01-higher-order-function.js
@@ -1,49 +1,68 @@
-// 高级函数-函数作为参数
-function forEach(array, fn) {
-  for (let i = 0; i < array.length; i++) {
-    fn(array[i])
-  }
-}
+// // 高级函数-函数作为参数
+// function forEach(array, fn) {
+//   for (let i = 0; i < array.length; i++) {
+//     fn(array[i])
+//   }
+// }
 
 
-// filter
-function filter(array, fn) {
-  const res = []
-  for (let i = 0; i < array.length; i++) {
-    if (fn(array[i])) {
-      res.push(array[i])
-    }
-  }
-  return res
-}
+// // filter
+// function filter(array, fn) {
+//   const res = []
+//   for (let i = 0; i < array.length; i++) {
+//     if (fn(array[i])) {
+//       res.push(array[i])
+//     }
+//   }
+//   return res
+// }
 
-// 测试
-const arr = [1, 2, 3, 4]
-console.log('foreach')
-forEach(arr, function (item) {
-  console.log(item)
-})
-console.log('filter')
-const res = filter(arr, function (item) {
-  return item > 2
-})
-console.log(res)
+// // 测试
+// const arr = [1, 2, 3, 4]
+// console.log('foreach')
+// forEach(arr, function (item) {
+//   console.log(item)
+// })
+// console.log('filter')
+// const res = filter(arr, function (item) {
+//   return item > 2
+// })
+// console.log(res)
 
-// 函数作为返回值
-console.log('函数作为返回值')
-function once(fn) {
-  let done = false
+// // 函数作为返回值
+// console.log('函数作为返回值')
+// function once(fn) {
+//   let done = false
+//   return function () {
+//     if (!done) {
+//       done = true
+//       return fn.apply(this, arguments)
+//     }
+//   }
+// }
+
+// const pay = once(function (money) {
+//   console.log(`支付了${money}`)
+// })
+// pay(5)
+// pay(5)
+// pay(5)
+
+function memoize(fn) {
+  const cache = new Map()
   return function () {
-    if (!done) {
-      done = true
-      return fn.apply(this, arguments)
-    }
+    let arg = JSON.stringify(arguments)
+    cache[arg] = cache[arg] || fn.apply(fn, arguments)
+    return cache[arg]
   }
 }
 
-const pay = once(function (money) {
-  console.log(`支付了${money}`)
-})
-pay(5)
-pay(5)
-pay(5)
+function getArea(n) {
+  console.log(n)
+  return n + 2
+}
+
+const fn = memoize(getArea)
+console.log(fn(3))
+console.log(fn(3))
+console.log(fn(3))
--- javascript-2023/01-function/README.md
+++ javascript-2023/01-function/README.md
@@ -46,3 +46,65 @@ pay(5)
 
 **高阶函数使我们可以只关心输入输出，不用关心内部的细节**
 
+## 闭包
+
+在一个函数内部可以调用外部函数作用域的成员变量
+
+```js
+function funcA() {
+  let a = 1
+  return function() {
+    console.log(a) // 这里就是调用了外部函数的成员变量
+  }
+}
+
+```
+
+## 纯函数
+
+**相同的输入**永远只有**相同的输出**
+
+```js
+function add(a, b) {
+  return a + b
+}
+```
+
+#### 纯函数的好处
+
+因为没有副作用，所以可以用来做缓存函数
+
+```js
+function memoize(fn) {
+  const cache = new Map()
+  return function () {
+    let arg = JSON.stringify(arguments)
+    cache[arg] = cache[arg] || fn.apply(fn, arguments)
+    return cache[arg]
+  }
+}
+```
+
+## 柯里化
+
+- 当一个函数有多个参数时，先传递一部分参数调用它
+- 返回新的函数接收剩余的参数
+
+```js
+// 普通函数
+function funcA(age) {
+  let min = 18
+  return age > 18
+}
+
+// 柯里化
+function funcB(min) {
+  return function(age) {
+    return age > min
+  }
+}
+
+const fc = funcB(18)
+fc(20)
+
+```
